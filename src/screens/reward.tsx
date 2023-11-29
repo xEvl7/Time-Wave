62b4510eb454f426fe19803ab2ca805f0f51c617
Reganchong <82707166+Reganchong@users.noreply.github.com>
2023-11-29 21:53:24 +0800

    reward new

--- src/screens/reward.tsx
+++ src/screens/reward.tsx
@@ -1,33 +1,118 @@
-import {TouchableOpacity, Alert,Pressable, StyleSheet, Text, View, Image } from "react-native";
+import {
+  TouchableOpacity, 
+  Alert,
+  Pressable, 
+  ScrollView ,
+  StyleSheet, 
+  Text, 
+  View, 
+  Image } from "react-native";
+
+
+import firestore from "@react-native-firebase/firestore";
+
 import auth from "@react-native-firebase/auth";
-
-
 import TextButton from "../components/TextButton";
 import HeaderText from "../components/text_components/HeaderText";
 import BackgroundImageBox from "../components/BackgroundImageBox";
 import ContentContainer from "../components/ContentContainer";
-//import ValidatedTextInput from "../components/ValidatedTextInput";
+import ValidatedTextInput from "../components/ValidatedTextInput";
 import { useForm } from "react-hook-form";
-//import { fetchUserData } from "../features/userSlice";
+import { fetchUserData } from "../features/userSlice";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Screen.types";
-//import { useAppDispatch, useAppSelector } from "../hooks";
+import { useAppDispatch, useAppSelector } from "../hooks";
 import {
-  ImageSourcePropType,
-  ImageStyle,
-  StyleProp,
-  ViewStyle,
+ImageSourcePropType,
+ImageStyle,
+StyleProp,
+ViewStyle,
 } from "react-native";
 import React from "react";
 
 let testpoint=200;
+
+/*
+interface Rewardsdetails {
+  name:string;
+  prices: number;
+}
+*/
+var db = firestore();
+
+
+
+var docRef = db.collection("Rewards");
+
+
+
+
+ 
+const showwhat = () =>{
+
+
+  docRef.where("RID","==","R3").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+
+        const Details = doc.data();
+
+        console.log(Details.name);
+
+    
+console.log(Details.price);
+//const reww = firestore().collection('Rewards').doc('Fan').collection('name');
+
+return Details;
+});
+
+})
+  /*
+    firestore().collection('Rewards').doc('R3').set({
+      category: "Category C",
+      supplierLogo: "URL to the image of the reward", //using Firebase Storage to store the actual image files and storing references (URLs) to these images in Cloud Firestore
+      supplierName: "Mr Jungle",
+      contactInfo: 'Mr Jungle',
+      highlight: "Incredible Straight",
+      image: "URL to the image of the reward", //using Firebase Storage to store the actual image files and storing references (URLs) to these images in Cloud Firestore
+      name: "Wood Stick(very straight)",
+      price: 10,
+      qtyAvailable: 1,
+      status: "active", //if this reward is discontinued but not delete from database then set to Discontinued (no longer available)
+      termsConditions: "Everyone can get it!!Be a Knight.Just do it. ",
+      validityStartDate: "1 Jan 2023",
+      validityEndDate: "30 Nov 2023",
+
+    category: "Category A"
+    supplierLogo: "URL to the image of the reward", //using Firebase Storage to store the actual image files and storing references (URLs) to these images in Cloud Firestore
+    supplierName: "Supplier 1",
+    contactInfo: 'Official Mavecap',
+    highlight: "Although, either structural comparison, based on sequence analysis or hardware maintenance boosts the growth of The Benefit of Explicative Capacity",
+    image: "URL to the image of the reward", //using Firebase Storage to store the actual image files and storing references (URLs) to these images in Cloud Firestore
+    name: "Singer Sewing Machine",
+    price: 100,
+    qtyAvailable: 5,
+    status: "active", //if this reward is discontinued but not delete from database then set to Discontinued (no longer available)
+    termsConditions: "Without a doubt, Barton Ashworth was right in saying that, the portion of the continuing support can be regarded as slowly insignificant.",
+    validityStartDate: 1 Nov 2023,
+    validityEndDate: 30 Nov 2023,
+*/
+
+ 
+    console.log('Working now!!!');
+
+
+
+
+}
+  
+
 
 
 
 
 
 const showTip = () => {
-  testpoint -=sew.itemprice;
+  testpoint -=100;
   Alert.alert(
 
     'Redeemed with 100 points!',
@@ -41,9 +126,10 @@ const showAlert = () =>{
   
   if (testpoint >= sew.itemprice )
   {
+
     Alert.alert(
     'Get This Reward!',
-    'Redeem with '+ sew.itemprice +' points?',
+    'Redeem with '+ 'huh?' +' points?',
     [
         {text: 'Cancel', style: 'cancel'},
         {text: 'Confirm', onPress: () => showTip()},
@@ -87,6 +173,7 @@ export default function Reward({
     </View>
       
       <ContentContainer>
+        
         <HeaderText>{sew.itemname}</HeaderText>
        
         <View style={styles.alternativesContainer}>
@@ -95,49 +182,66 @@ export default function Reward({
         
         <Text style={{ fontSize: 20 }}>{sew.itemprice} points</Text>
         </View>
-        
+        <View
+          style={styles.verticleLine
+          }
+          />{/*vertical line*/}
         <View style={styles.validityContainer}>
         <Text style={styles.boldtext}>Validity</Text>
     
         <Text style={{ fontSize: 20}}>{sew.validity}</Text>
         </View>
         </View>
+        <View
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+          />{/*line*/}
 
+        
 
           
+          
           <View style={styles.textContainer}>
+          
           <Text style={styles.boldtext}>Highlight</Text>
           <Text style={{ marginTop: 10, marginBottom: 10 }}>{sew.highlight} </Text>
           </View>
           <View style={styles.textContainer}>
           <Text style={styles.boldtext}>Terms & Conditions</Text>
           
-
           <Text style={{ marginTop: 5, marginBottom: 10 }}>{sew.term}</Text>
             
             
           </View>
           <View style={styles.textContainer}>
             
-            <Text style={styles.boldtext}>Contact Info</Text>
+            <Text style={styles.boldtext}>Contact  Info</Text>
   
             <Text style={{ marginTop: 5, marginBottom: 10 }}>{sew.contact} </Text>
               
               
             </View>
-
+            
 
         
 
 
         <View> 
 
-        
-            <TextButton style={{ marginTop: 20 ,marginBottom: 10, backgroundColor: "#FF8D13"}}  textStyle={styles.mainButtonText} onPress={showAlert}
+        <View
+          style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+          />{/*line*/}
+            <TextButton style={{ marginTop: 20 ,marginBottom: 10, backgroundColor: "#FF8D13"}}  textStyle={styles.mainButtonText} onPress={showwhat}
             >
             Redeem
             </TextButton>
           </View>
+          
 
 
       </ContentContainer>
@@ -184,11 +288,14 @@ const styles = StyleSheet.create({
   },//container
   pointContainer: {
     justifyContent: "space-evenly",
-    width: "30%",
+    width: "35%",
+    marginBottom:10,
   },
   validityContainer: {
     justifyContent: "space-evenly",
-    width: "70%",
+    width: "65%",
+    marginBottom:10,
+    marginLeft:20,
   },
   thirdPartyAuthContainer: {
     flexDirection: "row",
@@ -238,6 +345,11 @@ const styles = StyleSheet.create({
 
   mainButtonText: {
     color: "#06090C",
+  },
+  verticleLine: {
+    height: '100%',
+    width: 1,
+    backgroundColor: '#909090',
   },
 });
 
